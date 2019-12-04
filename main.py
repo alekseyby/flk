1ab5b2e94fbe4304dac4160256f54e7b89394b14
A Khramiankou <khromenkov@yandex.ru>
2019-12-04 23:50:19 +0300

    css added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     #  We imported render_template() method from the flask framework. render_template() looks for a template (HTML file) in the templates folder.
     return render_template("home.html")
-@app.route("/about)
+@app.route("/about")
 def about():
     return render_template("about.html")
 @app.route("/baam")
